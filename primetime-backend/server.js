88fe406c9a89a1e0f37806d96b483040f7748d6b
PKJ11 <108004611+PKJ11@users.noreply.github.com>
2025-05-17 18:34:41 +0530

    cors

--- primetime-backend/server.js
+++ primetime-backend/server.js
@@ -12,10 +12,13 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//   credentials: true,
+// }));
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  credentials: true,
-}));
+  origin: '*'
+}))
 app.use(express.json());
 
 const connectDB = require("./config/db");
