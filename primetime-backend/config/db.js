02f0e1e74a2272c1327a5a1986428459f6a2018c
PKJ11 <108004611+PKJ11@users.noreply.github.com>
2025-05-18 09:15:28 +0530

    Update db.js

--- primetime-backend/config/db.js
+++ primetime-backend/config/db.js
@@ -1,11 +1,19 @@
-const mongoose = require("mongoose");
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://pratikkumarjhavnit:cBkOwgGUuMB4ZMia@cluster0.sxfhet5.mongodb.net/PrimeTime?retryWrites=true&w=majority"
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://pratikkumarjhavnit:cBkOwgGUuMB4ZMia@cluster0.sxfhet5.mongodb.net/PrimeTime?retryWrites=true&w=majority");
-    console.log("MongoDB Connected");
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000
+    });
+    console.log('MongoDB Connected...');
   } catch (err) {
-    console.error("MongoDB Connection Error:", err.message);
+    console.error('MongoDB Connection Error:', err.message);
     process.exit(1);
   }
 };
